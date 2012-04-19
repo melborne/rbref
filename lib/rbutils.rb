427bd31f645ae01b7338911285a2bcd2a53aef46
kyoendo <postagie@gmail.com>
2012-04-20 07:54:39 +0900

    refactor

--- lib/rbutils.rb
+++ lib/rbutils.rb
@@ -3,89 +3,84 @@
 # rbutils.rb -
 # Author:: kyoendo
 
-#require "find"
-module RbUtils
-  def self.classes
-    objects(Class)
-  end
-  
-  def self.modules
-    objects(Module)
-  end
-
-  def self.objects(target)
-    ObjectSpace.each_object(Module).select { |obj| obj.class == target }
-  end
-  private :objects
-  
-  def self.constants
-    Module.constants.reject{ |c| [Class, Module].include? eval("#{c}").class }
-  end
-  
-  # depends on library paths
-  def self.standard_library
-    lib186 = %w(abbrev base64 benchmark bigdecimal cgi cgi-lib complex csv curses date date2 dbm debug delegate digest dl drb e2mmap English Env erb eregex etc expect fcntl fileutils finalize find forwardable ftools gdbm generator getoptlong getopts gserver iconv importenv io/nonblock io/wait ipaddr irb jcode kconv logger mailread mathn matrix md5 mkmf monitor mutex_m net/ftp net/ftptls net/http net/https net/imap net/pop net/protocol net/smtp net/telnet net/telnets nkf observer open3 open-uri openssl optparse ostruct parsearg parsedate pathname ping pp prettyprint profile profiler pstore pty racc/parser rational rbconfig readbytes readline resolv resolv-replace rexml rinda/rinda rinda/tuplespace rss rubyunit scanf sdbm set sha1 shell shellwords singleton soap socket stringio strscan sync syslog tempfile test/unit thread thwait time timeout tk tmpdir tracer tsort un uri weakref webrick win32/registry win32/resolv Win32API win32ole wsdl xmlrpc xsd yaml zlib)
-    lib187 = lib186 - ['securerandom']
-    lib191 = lib187 - %w(Env cgi-lib complex date2 eregex finalize ftools generator getopts importenv jcode mailread md5 net/ftptls net/telnets parsearg parsedate ping rational readbytes rubyunit sha1 soap wsdl xsd) + %w(cmath continuation coverage fiber json minitest/mock minitest/spec minitest/unit prime rake ripper rubygems ubygems)
-    lib192 = lib191 + ['objspace']
-    lib193 = lib192 + ['io/console']
-
-    case RUBY_VERSION
-    when '1.8.6' then lib186
-    when '1.8.7' then lib187
-    when '1.9.1' then lib191
-    when '1.9.2' then lib192
-    when '1.9.3' then lib193
-    end.sort
-  end
-  
-  RUBY186 = "~/.rbenv/versions/1.8.6-p420/bin/ruby"
-
-  def self.ruby186_methods
-    begin
-      return [] unless system("#{RUBY186} -v")
-
-      meths = %x(#{RUBY186} -rrbutils -e "#{<<CODE}")
-        meths = {}
-        klass_and_module = RbUtils.classes + RbUtils.modules - [RbUtils]
-        klass_and_module.each do |klass|
-          meths[klass.to_s] = klass.methods_by_type
-        end
-        p meths
-CODE
-      eval(meths)
-    rescue Exception => e
-    end
-  end
-end
+$added_methods = %w(hierarchy methods_by_type class_tree)
 
 class Module
   def hierarchy
     (self.superclass ? self.superclass.hierarchy : []).unshift(self)
   end
   
-  def methods_by_type(*args)
-    meths = {}
-    args = ['pu','pt','pr','pui','pti','pri'] if args.empty?
-    meth_types = {'pu' => 'public_methods', 'pt' => 'protected_methods', 'pr' => 'private_methods', 'pui' => 'public_instance_methods', 'pti' => 'protected_instance_methods', 'pri' => 'private_instance_methods'}
-    args.map!{ |arg| meth_types[arg] }.compact!
-    args.each do |mtype|
-      meths[mtype] = self.send(mtype, false)
-      #match with string or symbol
-      meths[mtype].reject!{ |m| m =~ /hierarchy|methods_by_type|class_tree/ }
+  def methods_by_type(mtypes=%w(public_methods protected_methods private_methods public_instance_methods protected_instance_methods private_instance_methods))
+    mtypes.inject({}) do |h, mtype|
+      h[mtype] = self.send(mtype, false).reject{ |m| $added_methods.include? m.to_s }
+      h
     end
-    meths
   end
 end
 
 class Array
   def class_tree(parents)
-    result = []
-    parents.each do |parent|
-      s_class = select{ |this| this.superclass == parent }
+    parents.inject([]) do |result, parent|
+      s_class = select { |this| this.superclass == parent }
       result << (parents.empty? ? [] : class_tree(s_class.reverse)) << parent
+      result
     end
-    result
+  end
+end
+
+module RbUtils
+  RUBY186 = "~/.rbenv/versions/1.8.6-p420/bin/ruby"
+  LIB186 = %w(abbrev base64 benchmark bigdecimal cgi cgi-lib complex csv curses date date2 dbm debug delegate digest dl drb e2mmap English Env erb eregex etc expect fcntl fileutils finalize find forwardable ftools gdbm generator getoptlong getopts gserver iconv importenv io/nonblock io/wait ipaddr irb jcode kconv logger mailread mathn matrix md5 mkmf monitor mutex_m net/ftp net/ftptls net/http net/https net/imap net/pop net/protocol net/smtp net/telnet net/telnets nkf observer open3 open-uri openssl optparse ostruct parsearg parsedate pathname ping pp prettyprint profile profiler pstore pty racc/parser rational rbconfig readbytes readline resolv resolv-replace rexml rinda/rinda rinda/tuplespace rss rubyunit scanf sdbm set sha1 shell shellwords singleton soap socket stringio strscan sync syslog tempfile test/unit thread thwait time timeout tk tmpdir tracer tsort un uri weakref webrick win32/registry win32/resolv Win32API win32ole wsdl xmlrpc xsd yaml zlib)
+  LIB187 = LIB186 - ['securerandom']
+  LIB191 = LIB187 - %w(Env cgi-lib complex date2 eregex finalize ftools generator getopts importenv jcode mailread md5 net/ftptls net/telnets parsearg parsedate ping rational readbytes rubyunit sha1 soap wsdl xsd) + %w(cmath continuation coverage fiber json minitest/mock minitest/spec minitest/unit prime rake ripper rubygems ubygems)
+  LIB192 = LIB191 + ['objspace']
+  LIB193 = LIB192 + ['io/console']
+
+  class << self
+    def classes
+      objects(Class)
+    end
+
+    def modules
+      objects(Module)
+    end
+
+    def objects(target)
+      ObjectSpace.each_object(Module).select { |obj| obj.class == target }
+    end
+
+    def all_constants
+      Module.constants.reject{ |c| [Class, Module].include? eval("#{c}").class } - constants
+    end
+
+    # depends on library paths
+    def standard_library
+      case RUBY_VERSION
+      when '1.8.6' then LIB186
+      when '1.8.7' then LIB187
+      when '1.9.1' then LIB191
+      when '1.9.2' then LIB192
+      when '1.9.3' then LIB193
+      end.sort
+    end
+
+    def ruby186_methods
+      begin
+        return [] unless system("#{RUBY186} -v")
+
+        meths = %x(#{RUBY186} -rrbutils -e "#{<<CODE}")
+          meths = {}
+          objects = RbUtils.classes + RbUtils.modules - [RbUtils]
+          objects.each do |klass|
+            meths[klass.to_s] = klass.methods_by_type
+          end
+          p meths
+CODE
+        eval(meths)
+      rescue => e
+        print "class => #{e.class}\nmessage => #{e.message}\nbacktrace => #{e.backtrace}\n"
+      end
+    end
   end
 end
 
