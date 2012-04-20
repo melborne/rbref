b04ec8b2b552fe804b2e09d2c85f568e50a24a9c
kyoendo <postagie@gmail.com>
2012-04-20 10:24:40 +0900

    build RbrefGenerator class

--- lib/rbref_gen.rb
+++ lib/rbref_gen.rb
@@ -6,176 +6,162 @@ $:.unshift(File.dirname(__FILE__))
 require "rbutils"
 require "erb"
 
-#RUBY_REF = "http://doc.okkez.net/#{RUBY_VERSION.delete('.')}/view/"
-RUBY_REF = "http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/"
-USEFUL_LINKS = 
-  { "Standard Library" => "#{RUBY_REF}library",
-    "Regular Expression" => "#{RUBY_REF}doc/spec=2fregexp.html",
-    "sprintf format" => "#{RUBY_REF}doc/print_format.html",
-    "strftime format" => "#{RUBY_REF}method/Time/i/strftime.html",
-    "Kernel module" => "#{RUBY_REF}class/Kernel.html",
-    "M17N" => "#{RUBY_REF}doc/spec=2fm17n.html",
-    "RubyGems" => "http://rubygems.org/",
-    "The Ruby Toolbox" => "https://www.ruby-toolbox.com/",
-    "Gem Docs" => "http://rubydoc.info/gems",
-    "Ruby Official" => "http://www.ruby-lang.org/ja/",
-    "Rubyist Magazine" => "http://jp.rubyist.net/magazine/",
-    "Ruby-list" => "http://blade.nagaokaut.ac.jp/ruby/ruby-list/index.shtml",
-    "Ruby-talk" => "http://blade.nagaokaut.ac.jp/ruby/ruby-talk/index.shtml",
-    "RHG" => "http://i.loveruby.net/ja/rhg/book/"
-   }
-RUBY_DESC = 
-  if RUBY_VERSION >= '1.8.7'
-    RUBY_DESCRIPTION
-  else
-    "ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE} patchlevel #{RUBY_PATCHLEVEL}) [#{RUBY_PLATFORM}]"
+class RbrefGenerator
+  RUBY_REF = "http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/"
+  DIRECT_LINKS = 
+    { "Standard Library" => "#{RUBY_REF}library",
+      "Regular Expression" => "#{RUBY_REF}doc/spec=2fregexp.html",
+      "sprintf format" => "#{RUBY_REF}doc/print_format.html",
+      "strftime format" => "#{RUBY_REF}method/Time/i/strftime.html",
+      "Kernel module" => "#{RUBY_REF}class/Kernel.html",
+      "M17N" => "#{RUBY_REF}doc/spec=2fm17n.html",
+    }
+  SITES =
+    { "RubyGems" => "http://rubygems.org/",
+      "The Ruby Toolbox" => "https://www.ruby-toolbox.com/",
+      "Gem Docs" => "http://rubydoc.info/gems",
+      "Ruby Official" => "http://www.ruby-lang.org/ja/",
+      "Rubyist Magazine" => "http://jp.rubyist.net/magazine/",
+      "Ruby-list" => "http://blade.nagaokaut.ac.jp/ruby/ruby-list/index.shtml",
+      "Ruby-talk" => "http://blade.nagaokaut.ac.jp/ruby/ruby-talk/index.shtml",
+      "RHG" => "http://i.loveruby.net/ja/rhg/book/"
+     }
+  RUBY_DESC = 
+    if RUBY_VERSION >= '1.8.7'
+      RUBY_DESCRIPTION
+    else
+      "ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE} patchlevel #{RUBY_PATCHLEVEL}) [#{RUBY_PLATFORM}]"
+    end
+  ENCODE_TBL = { "*" => "=2a", "+" => "=2b", "\\-" => "=2d", "." => "=2e", "/" => "=2f", ":" => "=3a", "<" => "=3c", "=" => "=3d", ">" => "=3e", "?" => "=3f", "\\[" => "=5b", "\\]" => "=5d", "\\^" => "=5e", "~" => "=7e", "|" => "=7c", "@" => "=40", "!" => "=21",  "%" => "=25", "&" => "=26"}
+  
+  REQUIRED_HERE = [:ERB, :StringScanner, :Gem, :RbConfig, :RbUtils, :RbrefGenerator]
+  
+  def self.method_added(name)
+    @@methods_here ||= []
+    @@methods_here << name
   end
-
-def Object.method_added(name)
-  @@methods_here ||= ['method_added']
-  @@methods_here << name
-end
-
-def method_list(klass)
-  methods = klass.methods_by_type
-  methods.keys.each do |key|
-    unless klass == Module && key == 'private_instance_methods'
-      methods[key].reject!{ |m| @@methods_here.include? m }
+  
+  def get_classes_and_error_classes
+    klasses = RbUtils.classes.reject { |klass| REQUIRED_HERE.any? { |ex| klass.to_s.match /^#{ex}/ } }.sort_by { |klass| klass.to_s }
+    klasses, errors = klasses.partition { |klass| klass.to_s !~ /Error/}
+    root = self.class.ancestors.select { |anc| anc.class == Class }.last
+    klasses = klasses.class_tree([root])
+    errors = errors.class_tree([Exception])
+    return [klasses, errors].map { |tree| tree.flatten.reverse }
+  end
+  
+  def get_modules
+    modules = RbUtils.modules.sort_by{ |k| k.to_s }.reject { |mod| REQUIRED_HERE.any? { |ex| mod.to_s.match /^#{ex}/ }}.sort_by { |mod| mod.to_s }
+  end
+  
+  def get_constants
+    (RbUtils.all_constants - self.class.constants).sort
+  end
+  
+  def get_libraries
+    RbUtils.standard_library
+  end
+  
+  def get_ruby186_methods
+    RbUtils.ruby186_methods
+  end
+  
+  def generate(path="../views/#{RUBY_VERSION.delete('.')}.erb")
+    File.open(path, "w") do |f|
+      f.puts ERB.new(DATA.read).result
     end
   end
-  return methods.reject{ |k,v| v.empty? }
-end
-
-def class_link(klass)
-  begin
-    unless RUBY186_METHODS.keys.to_s.include?(klass.to_s)
+  def method_list(klass)
+    methods = klass.methods_by_type
+    methods.keys.each do |key|
+      unless klass == Module && key == 'private_instance_methods'
+        methods[key].reject!{ |m| @@methods_here.include? m }
+      end
+    end
+    return methods.reject{ |k,v| v.empty? }
+  end
+  
+  def class_link(klass)
+    begin
+      unless RUBY186_METHODS.keys.to_s.include?(klass.to_s)
+        'class19_link'
+      else
+        'top_class_link'
+      end
+    rescue Exception => e
       'class19_link'
-    else
-      'top_class_link'
     end
-  rescue Exception => e
-    'class19_link'
   end
-end
-
-def method_link(klass, meth_type, meth)
-  begin
-    unless RUBY186_METHODS[klass.to_s][meth_type].include?(meth.to_s)
-      'meth19_link'
-    else
-      "meth_link"
+  
+  def method_link(klass, meth_type, meth)
+    begin
+      unless RUBY186_METHODS[klass.to_s][meth_type].include?(meth.to_s)
+        'meth19_link'
+      else
+        "meth_link"
+      end
+    rescue Exception => e
+      "meth19_link"
     end
-  rescue Exception => e
-    "meth19_link"
   end
-end
-
-# correct links for Enumerator Class
-def enum_class_check(klass)
-  if klass.to_s =~ /Enumerator/ and RUBY_VERSION <= "1.9.0"
-    'Enumerable=Enumerator'
-  else
-    klass
-  end
-  klass
-end
-
-ENCODE_TBL = { "*" => "=2a", "+" => "=2b", "\\-" => "=2d", "." => "=2e", "/" => "=2f", ":" => "=3a", "<" => "=3c", "=" => "=3d", ">" => "=3e", "?" => "=3f", "\\[" => "=5b", "\\]" => "=5d", "\\^" => "=5e", "~" => "=7e", "|" => "=7c", "@" => "=40", "!" => "=21",  "%" => "=25", "&" => "=26"}
-
-def h(url)
-  url = url.to_s.sub(/\.class$/, '') # handle errgular link for ARGF.class
-  url.gsub(/["#{ENCODE_TBL.keys.join}"]/) do |s|
-    s.sub!(/[\[\]\^\-]/) { |i| "\\#{i}" }
-    ENCODE_TBL[s]
-  end
-end
-
-def ruby_man_method_link(klass, meth_type, meth)
-  case
-  when klass == Kernel && meth_type == "public_instance_methods"
-    klass = Object
-    mtype = "i"
-  when klass.class == Module
-    if meth_type !~ /instance/
-      klass = Module
-      mtype = "i"
-    elsif meth_type == 'private_instance_methods'
-      mtype = "m"
-    else #'public_instance_methods'
-      mtype = "i"
+  
+  def h(url)
+    url = url.to_s.sub(/\.class$/, '') # handle errgular link for ARGF.class
+    url.gsub(/["#{ENCODE_TBL.keys.join}"]/) do |s|
+      s.sub!(/[\[\]\^\-]/) { |i| "\\#{i}" }
+      ENCODE_TBL[s]
     end
-  when meth_type == 'public_methods'
-    if meth =~ /allocate|new|superclass/
-      klass = Class
+  end
+  
+  def ruby_man_method_link(klass, meth_type, meth)
+    case
+    when klass == Kernel && meth_type == "public_instance_methods"
+      klass = Object
       mtype = "i"
-    else
+    when klass.class == Module
+      if meth_type !~ /instance/
+        klass = Module
+        mtype = "i"
+      elsif meth_type == 'private_instance_methods'
+        mtype = "m"
+      else #'public_instance_methods'
+        mtype = "i"
+      end
+    when meth_type == 'public_methods'
+      if meth =~ /allocate|new|superclass/
+        klass = Class
+        mtype = "i"
+      else
+        mtype = "s"
+      end
+    when meth_type =~ /private/
+      klass = Object unless klass == Module
+      mtype = "i"
+    when klass == ARGF.class
+      klass = ARGF
       mtype = "s"
+    else # public_instance_methods
+      mtype = "i"
     end
-  when meth_type =~ /private/
-    klass = Object unless klass == Module
-    mtype = "i"
-  when klass == ARGF.class
-    klass = ARGF
-    mtype = "s"
-  else # public_instance_methods
-    mtype = "i"
+    RUBY_REF + "method/#{h klass}" + "/#{mtype}/#{h meth}.html"
   end
-  #meth = meth.to_s.gsub(/["#{ENCODE_TBL.keys.join}"]/) do |s|
-    #s.sub!(/[\[\]\^\-]/) { |i| "\\#{i}" }
-    #ENCODE_TBL[s]
-  #end
-  RUBY_REF + "method/#{klass}" + "/#{mtype}/#{h meth}.html"
+  
 end
 
-REQUIRED_CLASSES = [ERB, RbUtils]
-DEFINED_HERE = [:RUBY_REF, :RUBY186_METHODS, :RUBY_DESC, :USEFUL_LINKS, :ENCODE_TBL, :RUBY186, :DEFINED_HERE, :REQUIRED_CLASSES ]
-def get_classes
-  klasses = RbUtils.classes.sort_by{|k| k.to_s }.reject { |k| REQUIRED_CLASSES.any? { |ex| k.to_s.match /^#{ex}/ } }
-  if RUBY_VERSION >= "1.9.0"
-    klasses -= [Complex, RubyVM]
-    sclass = BasicObject
-  else
-    klasses -= [StringScanner, StringScanner::Error]
-    sclass = Object
-  end
-  klasses, errors = klasses.partition{|k| k.to_s !~ /Error/}
-  klasses = klasses.class_tree([sclass])
-  errors = errors.class_tree([Exception])
-  return [klasses, errors].map { |tree| tree.flatten.reverse }
-end
+rb = RbrefGenerator.new
 
-def get_modules
-  modules = RbUtils.modules.sort_by{ |k| k.to_s } - REQUIRED_CLASSES
-  modules -= RUBY_VERSION >= "1.9.0" ? [Gem] : []
-end
-
-def get_constants
-  RbUtils.constants - DEFINED_HERE
-end
-
-def get_libraries
-  RbUtils.standard_library
-end
-
-def get_ruby186_methods
-  RbUtils.ruby186_methods
-end
-
-def generate(path="../views/#{RUBY_VERSION.delete('.')}.erb")
-  File.open(path, "w") do |f|
-    f.puts ERB.new(DATA.read).result
-  end
-end
-
-klasses, error_klasses = get_classes
-modules = get_modules
+klasses, error_klasses = rb.get_classes_and_error_classes
+modules = rb.get_modules
 klass_and_module = klasses + modules + error_klasses
-constants = get_constants
-libraries = get_libraries
-RUBY186_METHODS = get_ruby186_methods
+constants = rb.get_constants
+libraries = rb.get_libraries
+RUBY186_METHODS = rb.get_ruby186_methods
+RUBY_REF = RbrefGenerator::RUBY_REF
+RUBY_DESC = RbrefGenerator::RUBY_DESC
+DIRECT_LINKS = RbrefGenerator::DIRECT_LINKS
+SITES = RbrefGenerator::SITES
 
-
-generate()
+rb.generate()
 
 __END__
  <div id='top'>
@@ -186,34 +172,40 @@ __END__
  <div class='top_list'>
    <h3 class='top_subtitle'>Class</h3>
    <% klasses.each do |klass| %>
-   <span><a class=<%= class_link(klass) %> href="#<%= klass %>"><%= klass %></a> | </span>
+   <span><a class=<%= rb.class_link(klass) %> href="#<%= klass %>"><%= klass %></a> | </span>
    <% end %>
    <span class="counter"><%= klasses.length %></span>
  </div>
  <div class='top_list'>
    <h3 class='top_subtitle'>Module</h3>
    <% modules.each do |mod| %>
-   <span><a class=<%= class_link(mod) %> href="#<%= mod %>"><%= mod %></a> | </span>
+   <span><a class=<%= rb.class_link(mod) %> href="#<%= mod %>"><%= mod %></a> | </span>
    <% end %>
    <span class="counter"><%= modules.length %></span>
  </div>
  <div class='top_list'>
    <h3 class='top_subtitle'>Exception Class</h3>	    
    <% error_klasses.each do |error| %>
-   <span><a class=<%= class_link(error) %> href="#<%= error %>"><%= error %></a> | </span>
+   <span><a class=<%= rb.class_link(error) %> href="#<%= error %>"><%= error %></a> | </span>
    <% end %>
    <span class="counter"><%= error_klasses.length %></span>
  </div>
  <div class='top_list'>
    <h3 class='top_subtitle'>Standard Library</h3>	    
    <% libraries.each do |lib| %>
-   <span><a class="top_class_link" href="<%= RUBY_REF %>library/<%= h(lib) %>.html"><%= lib %></a> | </span>
+   <span><a class="top_class_link" href="<%= RUBY_REF %>library/<%= rb.h(lib) %>.html"><%= lib %></a> | </span>
    <% end %>
    <span class="counter"><%= libraries.length %></span>
  </div>
  <div class='top_list'>
-   <h3 class='top_subtitle'>Links</h3>	    
-   <% USEFUL_LINKS.each do |name, site| %>
+   <h3 class='top_subtitle'>Direct Links</h3>	    
+   <% DIRECT_LINKS.each do |name, site| %>
+   <span><a class="top_class_link" href=<%= site %>><%= name %></a> | </span>
+   <% end %>
+ </div>
+ <div class='top_list'>
+   <h3 class='top_subtitle'>Sites</h3>	    
+   <% SITES.each do |name, site| %>
    <span><a class="top_class_link" href=<%= site %>><%= name %></a> | </span>
    <% end %>
  </div>
@@ -228,7 +220,7 @@ __END__
      <a href="">top</a>
    </span>
    <div class='class_title_bar' id="<%= klass %>">
-     <a class="class_link" id='class_name' href="<%= RUBY_REF %>class/<%= h klass %>.html"><%= klass %></a><span id='class_suffix'> <%= klass.class %></span>
+     <a class="class_link" id='class_name' href="<%= RUBY_REF %>class/<%= rb.h(klass) %>.html"><%= klass %></a><span id='class_suffix'> <%= klass.class %></span>
      <span class="superclass">
        <% unless super_classes.empty? %>
          <% super_classes.each do |sc| %>
@@ -246,11 +238,11 @@ __END__
    </div>
  
    <div class='meths'>
-     <% method_list(klass).each do |meth_type, meths| %>
+     <% rb.method_list(klass).each do |meth_type, meths| %>
        <h3 class='meth_type'><%= meth_type %></h3>
        <% meths.sort.each_with_index do |meth, i| %>
          <span>
-           <a class=<%= method_link(klass,meth_type,meth) %> href=<%= ruby_man_method_link(klass,meth_type,meth) %>><%= meth %></a>
+           <a class=<%= rb.method_link(klass,meth_type,meth) %> href=<%= rb.ruby_man_method_link(klass,meth_type,meth) %>><%= meth %></a>
            <%= i+1 < meths.length ? " | " : "" %>
          </span>
        <% end %>
